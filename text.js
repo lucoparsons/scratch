4ac1ac9cd057fc5ad152db8d148bb35c81fb36ef
Matt Albinson <mattalbinson@berkeley.net>
2017-03-03 09:38:22 -0800

    Added 2D array for images, and all of the turning and walking basic functions!

--- text.js
+++ text.js
@@ -46,7 +46,6 @@ function buildInitialArray(result) {
     }
 }
 
-
 function appendArray() {
     var z = 0;
     for (var x=0; x<10; x++) {
@@ -54,76 +53,107 @@ function appendArray() {
         for (var y=0; y<10; y++) {
             resultArray2d[x][y] = resultArray[z];
             z += 1;
-            $("#img").append("<img src='" + resultArray2d[x][y].link + "' width='100' height='100'>")
         }
     }
     console.log(resultArray2d);
 }
 
-
-var forest = {
-    x0: {
-        y0: {
-            E: 'imgs/creekSample.jpg',
-            S: 'imgs/pathSample.jpg'
-        },
-        y1: {
-            E: 'https://i.ytimg.com/vi/jF7TqzPP38A/hqdefault.jpg',
-            S: 'cabinSample.jpg',
-            W: ''
-        }
-    }
-};
-console.log(forest.x0.y0.E);
-
 var facing = ['N', 'E', 'S', 'W'];
 var a = 0;
 var direction = facing[0];
 
+
+function turnyImages() {
+    if (direction == 'N') {
+        $('#backdrop').fadeOut(function () {
+            $(this).attr('src', resultArray2d[x][y+1].link).fadeIn();
+        });
+    }
+    else if (direction == 'E') {
+        $('#backdrop').fadeOut(function () {
+            $(this).attr('src', resultArray2d[x+1][y].link).fadeIn();
+        });
+    }
+    else if (direction == 'S') {
+        $('#backdrop').fadeOut(function () {
+            $(this).attr('src', resultArray2d[x][y-1].link).fadeIn();
+        });
+    }
+    else if (direction == 'W') {
+        $('#backdrop').fadeOut(function () {
+            $(this).attr('src', resultArray2d[x-1][y].link).fadeIn();
+        });
+    }
+}
+x=4;
+y=4;
 var turnRight = function() {
     $(document).ready(function() {
-        $('#backdrop').fadeOut(function () {
-            $(this).attr('src', forest.x0y0F).fadeIn();
-        });
         a += 1;
         if (a > 3) {
             a -= 4;
         }
         direction = facing[a];
         document.getElementById("compass").src = "imgsCompass/" + direction + ".png";
-        console.log(a);
-        console.log(direction);
-        console.log(resultArray);
-        console.log(resultArray2);
+        turnyImages();
     });
 };
 
 var turnLeft = function() {
     $(document).ready(function() {
-        $('#backdrop').fadeOut(function () {
-            $(this).attr('src', forest.x0y0F).fadeIn();
-        });
         a += 3;
         if (a > 3) {
             a -= 4;
         }
         direction = facing[a];
         document.getElementById("compass").src = "imgsCompass/" + direction + ".png";
-        console.log(a);
-        console.log(direction);
+        turnyImages();
     });
 };
 
 var walk = function() {
-
+    if (direction == 'N') {
+        y += 1;
+        $('#backdrop').fadeOut(function () {
+            $(this).attr('src', resultArray2d[x][y+1].link).fadeIn();
+        });
+    }
+    else if (direction = 'E') {
+        x+=1;
+        $('#backdrop').fadeOut(function () {
+            $(this).attr('src', resultArray2d[x+1][y].link).fadeIn();
+        });
+    }
+    else if (direction = 'S') {
+        y-=1;
+        $('#backdrop').fadeOut(function () {
+            $(this).attr('src', resultArray2d[x][y-1].link).fadeIn();
+        });
+    }
+    else if (direction = 'W') {
+        x-=1;
+        $('#backdrop').fadeOut(function () {
+            $(this).attr('src', resultArray2d[x-1][y].link).fadeIn();
+        });
+    }
+    document.getElementById("backdrop").src = resultArray2d[x][y].link
 };
 
 
-/*function returnObj(result) {
-    resultObj = result;
-    //console.log(resultObj)
-}*/
 
-function handler(imgTag) {
-    document.getElementById("backdrop").src=resultObj.items[imgTag].link;
+/*var forest = {
+x0: {
+    y0: {
+        E: 'imgs/creekSample.jpg',
+            S: 'imgs/pathSample.jpg'
+    },
+    y1: {
+        E: 'https://i.ytimg.com/vi/jF7TqzPP38A/hqdefault.jpg',
+            S: 'cabinSample.jpg',
+            W: ''
+    }
 }
+};
+console.log(forest.x0.y0.E);*/
+
+    //appending sum imgs: $("#img").append("<img src='" + resultArray2d[x][y].link + "' width='100' height='100'>")
